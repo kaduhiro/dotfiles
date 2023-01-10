d5d7acaf18ba6e49615b4682604784e31144820d
kaduhiro <github@kaduhiro.com>
2023-01-10 11:26:16 +0000

    fix: remove unnecessary output

--- default/.profile.d/tig.sh
+++ default/.profile.d/tig.sh
@@ -1,3 +1,3 @@
 # tig
 alias tig='tig --all'
-alias .tig='cd ~/.dotapps/home/dotfiles@kaduhiro && tig --all && cd -'
+alias .tig='cd ~/.dotapps/home/dotfiles@kaduhiro && tig --all && cd - > /dev/null'
