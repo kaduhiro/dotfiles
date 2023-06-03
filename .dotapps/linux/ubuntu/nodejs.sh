55262f22d4def491fafe2e1e09d3f6dff28ba6d3
kaduhiro <github@kaduhiro.com>
2023-06-03 17:41:26 +0900

    feat: add script to install Node.js

---
+++ .dotapps/linux/ubuntu/nodejs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+[ "$OSNAME" = 'linux' ]
+[ "$OSDIST" = 'ubuntu' ]
+
+if ! type nodejs; then
+	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+	sudo apt-get install -y nodejs
+fi
