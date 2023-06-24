cc7d5e3a7715330f2ed4cdc1a540a81b53fa7d73
kaduhiro <github@kaduhiro.com>
2023-06-25 06:38:37 +0900

    feat: add alias to run tig in any directory

--- default/.profile.d/tig.sh
+++ default/.profile.d/tig.sh
@@ -1,3 +1,4 @@
 # tig
 alias tig='tig --all'
-alias .tig='cd ~/.dotapps/home/dotfiles@kaduhiro && tig --all && cd - > /dev/null'
+alias tigd='f(){d=${1:-.} && cd "$d" && tig && cd - > /dev/null};f'
+alias .tig='cd ~/.dotapps/home/dotfiles@kaduhiro && tig && cd - > /dev/null'
