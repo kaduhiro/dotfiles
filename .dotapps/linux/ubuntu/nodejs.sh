b0bb5c47b346f8d553f5b4b71e4941b107f0ccf8
kaduhiro <github@kaduhiro.com>
2023-09-02 17:13:07 +0900

    chore: update Node.js version 18 to 20

--- .dotapps/linux/ubuntu/nodejs.sh
+++ .dotapps/linux/ubuntu/nodejs.sh
@@ -3,6 +3,14 @@
 [ "$OSDIST" = 'ubuntu' ]
 
 if ! type nodejs; then
-	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+	NODE_MAJOR=20
+
+	sudo apt-get update
+	sudo apt-get install -y ca-certificates curl gnupg
+
+	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+	sudo apt-get update
 	sudo apt-get install -y nodejs
 fi
