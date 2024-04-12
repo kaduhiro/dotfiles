63c0fa402e012ca970d2f6634cdcc6122ef5cf8c
kaduhiro <github@kaduhiro.com>
2024-04-13 06:12:48 +0900

    feat: add HomeBrew installation

--- .dotapps/macos/homebrew.sh
+++ .dotapps/macos/homebrew.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 [ "$OSNAME" = 'macos' ]
 
+# install Homebrew
+if ! command brew; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	export PATH=$PATH:/opt/homebrew/bin
+fi
+
 # install applications
 cd
 brew bundle
