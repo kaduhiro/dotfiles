5a8b3a962e0ba4fe078523b617d0616016b778b0
kaduhiro <github@kaduhiro.com>
2026-05-09 05:19:21 +0900

    fix: update sudoers script to check for existing pam_reattach configuration

--- .dotapps/macos/sudoers.sh
+++ .dotapps/macos/sudoers.sh
@@ -2,4 +2,10 @@
 [ "$OSNAME" = 'macos' ]
 
 # Enable sudo by Touch ID
-sed -e 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
+# - https://qiita.com/kawaz/items/0593163c1c5538a34f6f
+if grep -Eq '^auth[ \t].*[ \t/]pam_reattach\.so$' /etc/pam.d/sudo_local; then
+	exit 1
+fi
+
+# sed -e 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
+sh -c "$(curl -fsLS https://gist.githubusercontent.com/kawaz/d95fb3b547351e01f0f3f99783180b9f/raw/10c9e17e62dd3f08d3e564cad69324f6f2297380/install-pam_tid-and-pam_reattach.sh)"
